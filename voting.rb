3f74a68cf6ef35befac80e017194c1f6059e0869
sa2taka <sa2taka@gmail.com>
2017-04-09 21:22:08 +0900

    Become init instance variables

--- voting.rb
+++ voting.rb
@@ -3,9 +3,13 @@ require 'singleton'
 class Voting
   include Singleton
   attr_accessor :human_voting_place, :wolf_voting_place, :normal_voting_place
+  @human_voting_place = {}
+  @wolf_voting_place = {}
+  @normal_voting_place = {}
+
   def reset
-    @human_voting_place = 0
-    @wolf_voting_place = 0
-    @normal_voting_place = 0
+    @human_voting_place = {}
+    @wolf_voting_place = {}
+    @normal_voting_place = {}
   end
 end
